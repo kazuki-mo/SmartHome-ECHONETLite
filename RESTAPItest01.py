6f5fdc7df9504a9ba0d778740aceaefa72b5ec87
kazuki-mo <moriya.kazuki.me0@is.naist.jp>
2016-01-11 16:14:48 +0900

    冷蔵庫、IH、TV、照明のAPI追加

--- RESTAPItest01.py
+++ RESTAPItest01.py
@@ -9,24 +9,13 @@ from flask import Flask, jsonify, request, url_for, abort, Response
 
 app = Flask(__name__)
 
-# ダミーデータ (DB の代わり)
-id_index = 3
-users = {1: {'id': 1, 'name': 'foo'},
-         2: {'id': 2, 'name': 'bar'}}
-
-
-def consumes(content_type):
-    def _consumes(function):
-        @wraps(function)
-        def __consumes(*argv, **keywords):
-            if request.headers['Content-Type'] != content_type:
-                abort(400)
-            return function(*argv, **keywords)
-        return __consumes
-    return _consumes
-
 AirconBR_IP = "192.168.126.104"
 AirconLR_IP = "192.168.126.103"
+TV_IP = "192.168.126.105"
+IH_IP = "192.168.126.106"
+Refrige_IP = "192.168.126.107"
+Light_IP = "192.168.126.101"
+
 
 ############################ Aircon_BR #######################################
 @app.route('/Aircon_BR/Power/<value>')
@@ -63,7 +52,7 @@ def AirconBR_DriveMode(value):
 
 @app.route('/Aircon_BR/SetTemp/<value>')
 def AirconBR_SetTemp(value):
-    value = format(int(value), 'x')
+    value = format(int(value), '02x')
     cmd = "./OneShotSender " + AirconBR_IP + " 0 0001 0ef001 013001 61 B3 " + value
     subprocess.call(cmd.split(" "))
     cmd = "./OneShotSender -r " + AirconBR_IP + " 0 0001 0ef001 013001 62 B3 ."
@@ -75,7 +64,7 @@ def AirconBR_AirFlow(value):
     if value == "Auto":
         value = "41"
     else:
-        value = format(int(value)+48, 'x')
+        value = format(int(value)+48, '02x')
     cmd = "./OneShotSender " + AirconBR_IP + " 0 0001 0ef001 013001 61 A0 " + value
     subprocess.call(cmd.split(" "))
     cmd = "./OneShotSender -r " + AirconBR_IP + " 0 0001 0ef001 013001 62 A0 ."
@@ -118,7 +107,7 @@ def AirconLR_DriveMode(value):
 
 @app.route('/Aircon_LR/SetTemp/<value>')
 def AirconLR_SetTemp(value):
-    value = format(int(value), 'x')
+    value = format(int(value), '02x')
     cmd = "./OneShotSender " + AirconLR_IP + " 0 0001 0ef001 013001 61 B3 " + value
     subprocess.call(cmd.split(" "))
     cmd = "./OneShotSender -r " + AirconLR_IP + " 0 0001 0ef001 013001 62 B3 ."
@@ -130,95 +119,185 @@ def AirconLR_AirFlow(value):
     if value == "Auto":
         value = "41"
     else:
-        value = format(int(value)+48, 'x')
+        value = format(int(value)+48, '02x')
     cmd = "./OneShotSender " + AirconLR_IP + " 0 0001 0ef001 013001 61 A0 " + value
     subprocess.call(cmd.split(" "))
     cmd = "./OneShotSender -r " + AirconLR_IP + " 0 0001 0ef001 013001 62 A0 ."
     check = subprocess.check_output(cmd.split(" "))
     return str(check)
 
-@app.route('/')
-def hello():
-    global users
-    print(users)
-    return str(users)
 
-@app.route('/users', methods=['GET'])
-def index():
-    # ユーザ一覧からレスポンスを作る
-    response = jsonify({'results': users.values()})
-    # ステータスコードは OK (200)
-    response.status_code = 200
-    return response
-
-
-@app.route('/users', methods=['POST'])
-@consumes('application/json')
-def create():
-    global id_index
-    # Content-Body を JSON 形式として辞書に変換する
-    content_body_dict = json.loads(request.data)
-    # ID を付与する
-    content_body_dict['id'] = id_index
-    # ID をインクリメントする
-    id_index += 1
-    # ユーザ一覧に追加する
-    users[content_body_dict['id']] = (content_body_dict)
-    # レスポンスオブジェクトを作る
-    response = jsonify(content_body_dict)
-    # ステータスコードは Created (201)
-    response.status_code = 201
-    # 作成したリソースを Location ヘッダを設定する
-    response.headers['Location'] = url_for('read',
-                                           user_id=content_body_dict['id'])
-    return response
+############################ TV #######################################
+@app.route('/TV/Power/<value>')
+def TV_Power(value):
+    if value == "On":
+        value = "30"
+    elif value == "Off":
+        value = "31"
+    cmd = "./OneShotSender " + TV_IP + " 0 0001 0ef001 060201 61 80 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + TV_IP + " 0 0001 0ef001 060201 62 80 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
 
 
-def _get_user(user_id):
-    user = users.get(user_id)
-    if user is None:
-        abort(404)
-    return user
+############################ IH #######################################
+@app.route('/IH/Power/<value>')
+def IH_Power(value):
+    if value == "On":
+        value = "30"
+    elif value == "Off":
+        value = "31"
+    cmd = "./OneShotSender " + IH_IP + " 0 0001 0ef001 03B901 61 80 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + IH_IP + " 0 0001 0ef001 03B901 62 80 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
+
+@app.route('/IH/AllStop')
+def IH_AllStop():
+    cmd = "./OneShotSender " + IH_IP + " 0 0001 0ef001 03B901 61 B3 40"
+    subprocess.call(cmd.split(" "))
+    return "Success"
 
 
-@app.route('/users/<user_id>', methods=['GET'])
-def read(user_id):
-    # リクエストされたパスと ID を持つユーザを探す
-    found_user = _get_user(user_id)
-    # レスポンスオブジェクトを作る
-    response = jsonify(found_user)
-    # ステータスコードは Created (201)
-    response.status_code = 200
-    return response
+############################ Light #######################################
+@app.route('/Light/Power/<value>')
+def Light_Power(value):
+    if value == "On":
+        value = "30"
+    elif value == "Off":
+        value = "31"
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 80 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 80 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
 
+@app.route('/Light/LightLevel/<value>')
+def Light_LightLevel(value):
+    value = format(int(value), '02x')
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 B0 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 B0 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
 
-@app.route('/users/<user_id>', methods=['PUT'])
-@consumes('application/json')
-def update(user_id):
-    # Content-Body を JSON 形式として辞書に変換する
-    content_body_dict = json.loads(request.data)
-    # リクエストされたパスと ID を持つユーザを探す
-    found_user = _get_user(user_id)
-    # ユーザ名を書き換える
-    found_user['name'] = content_body_dict['name']
-    # レスポンスオブジェクトを作る
-    response = jsonify(found_user)
-    # ステータスコードは Created (201)
-    response.status_code = 200
-    return response
+@app.route('/Light/LightColor/<value>')
+def Light_LightColor(value):
+    if value == "LightBulb":
+        value = "41"
+    elif value == "White":
+        value = "42"
+    elif value == "NaturalWhite":
+        value = "43"
+    elif value == "Daylight":
+        value = "44"
+    elif value == "Other":
+        value = "40"
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 B1 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 B1 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
 
+@app.route('/Light/LightLevelStage/<value>')
+def Light_LightLevelStage(value):
+    value = format(int(value), '02x')
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 B2 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 B2 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
 
-@app.route('/users/<user_id>', methods=['DELETE'])
-def delete(user_id):
-    # リクエストされたパスと ID を持つユーザを探す
-    _get_user(user_id)
-    # ユーザがいれば削除する
-    users.pop(user_id)
-    # レスポンスオブジェクトを作る
-    response = Response()
-    # ステータスコードは NoContent (204)
-    response.status_code = 204
-    return response
+@app.route('/Light/LightColorStage/<value>')
+def Light_LightColorStage(value):
+    value = format(int(value), '02x')
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 B3 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 B3 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
+
+@app.route('/Light/LightingMode/<value>')
+def Light_LightingMode(value):
+    if value == "Auto":
+        value = "41"
+    elif value == "Normal":
+        value = "42"
+    elif value == "Night":
+        value = "43"
+    elif value == "Color":
+        value = "45"
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 B6 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 B6 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
+
+@app.route('/Light/LightLevel_Normal/<value>')
+def Light_LightLevelNormal(value):
+    value = format(int(value), '02x')
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 B7 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 B7 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
+
+@app.route('/Light/LightLevelStage_Normal/<value>')
+def Light_LightLevelStageNormal(value):
+    value = format(int(value), '02x')
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 B8 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 B8 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
+
+@app.route('/Light/LightLevel_Night/<value>')
+def Light_LightLevelNight(value):
+    value = format(int(value), '02x')
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 B9 " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 B9 ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
+
+@app.route('/Light/LightLevelStage_Night/<value>')
+def Light_LightLevelStageNight(value):
+    value = format(int(value), '02x')
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 BA " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 BA ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
+
+@app.route('/Light/LightColor_Normal/<value>')
+def Light_LightColor_Normal(value):
+    if value == "LightBulb":
+        value = "41"
+    elif value == "White":
+        value = "42"
+    elif value == "NaturalWhite":
+        value = "43"
+    elif value == "Daylight":
+        value = "44"
+    elif value == "Other":
+        value = "40"
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 BB " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 BB ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
+
+@app.route('/Light/LightColorStage_Normal/<value>')
+def Light_LightColorStage_Normal(value):
+    value = format(int(value), '02x')
+    cmd = "./OneShotSender " + Light_IP + " 0 0001 0ef001 029001 61 BC " + value
+    subprocess.call(cmd.split(" "))
+    cmd = "./OneShotSender -r " + Light_IP + " 0 0001 0ef001 029001 62 BC ."
+    check = subprocess.check_output(cmd.split(" "))
+    return str(check)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
